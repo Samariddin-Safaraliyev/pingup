9713c817419daa02485fe586d68cc81892dbe086
safaraliyevsamariddin973@gmail.com <safaraliyevsamariddin973@gmail.com>
2026-02-05 12:52:48 +0500

    profilemodal bio section

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import Loading from "../components/Loading";
 import UserProfileInfo from "../components/UserProfileInfo";
 import PostCard from "../components/PostCard";
 import moment from "moment";
+import ProfileModal from "./ProfileModal";
 
 const TABS = ["posts", "media", "likes"];
 
@@ -107,7 +108,7 @@ const Profile = () => {
             </div>
           </div>
           {/* edit profile modal */}
-          {showEdit && <p>show profile edit</p>}
+          {showEdit && <ProfileModal setShowEdit={setShowEdit} />}
         </div>
       ) : (
         <Loading />
